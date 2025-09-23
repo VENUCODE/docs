6cc20f04b14f0429850dfc671b167b93ff8f8bdc
yedukondalunaik <yedukondalunaik12345@gmail.com>
2025-09-23 23:00:21 +0530

    Add Tars widget script to custom.js for enhanced functionality: integrated script loading for Tars widget to improve user interaction. Update docs.json to include new thumbnail and background settings for better visual presentation. Modify platform-tour.mdx to add sidebar title for improved navigation and user experience.

--- custom.js
+++ custom.js
@@ -48,3 +48,4 @@ window.addEventListener("popstate", () => {
     // console.log("[popstate fired]", window.location.href);
     delayedOverride();
 });
+(function () { var js, fs, d = document, id = "tars-widget-script", b = "https://tars-file-upload.s3.amazonaws.com/bulb/"; if (!d.getElementById(id)) { js = d.createElement("script"); js.id = id; js.type = "text/javascript"; js.src = b + "js/widget.js"; fs = d.getElementsByTagName("script")[0]; fs.parentNode.insertBefore(js, fs) } })(); window.tarsSettings = { "convid": "1sfwNs" };
