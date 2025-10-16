c8369eef2c3ffc5356938d09f39d6e0a01790bd4
yedukondalunaik <yedukondalunaik12345@gmail.com>
2025-10-16 18:59:52 +0530

    Hiding the AI Agent for now, to add the trained the Agent

--- custom.js
+++ custom.js
@@ -51,18 +51,18 @@ window.addEventListener("popstate", () => {
 
 
 //function to add the chatbot to docs
-(function () {
-  var js,fs,d = document,id = "tars-widget-script",b = "https://tars-file-upload.s3.amazonaws.com/bulb/";
-  if (!d.getElementById(id)) {
-    js = d.createElement("script");
-    js.id = id;
-    js.type = "text/javascript";
-    js.src = b + "js/widget.js";
-    fs = d.getElementsByTagName("script")[0];
-    fs.parentNode.insertBefore(js, fs);
-  }
-})();
-window.tarsSettings = { convid: "1sfwNs" };
+// (function () {
+//   var js,fs,d = document,id = "tars-widget-script",b = "https://tars-file-upload.s3.amazonaws.com/bulb/";
+//   if (!d.getElementById(id)) {
+//     js = d.createElement("script");
+//     js.id = id;
+//     js.type = "text/javascript";
+//     js.src = b + "js/widget.js";
+//     fs = d.getElementsByTagName("script")[0];
+//     fs.parentNode.insertBefore(js, fs);
+//   }
+// })();
+// window.tarsSettings = { convid: "1sfwNs" };
 
 //trigger chatbot open when clicking on other elements
 
